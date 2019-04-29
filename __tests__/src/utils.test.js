b0a86f493eb1e96e57bc6cdeabad8394a553e2fc
Ted Weiss <tedweiss@Teds-MacBook-Pro.local>
2019-04-28 23:48:14 -0400

    calculateClockOutTime test should return the hour someone should clock out based on an 8 hour day

--- __tests__/src/utils.test.js
+++ __tests__/src/utils.test.js
@@ -1,7 +1,13 @@
-import { calculateClockOutHours } from '../../src/utils'
+import { calculateClockOutHours, calculateClockOutTime } from '../../src/utils'
 
 describe('calculateClockOutHours', () => {
   test('should return the hour someone should clock out based on an 8 hour day', () => {
     expect(calculateClockOutHours(9)).toEqual(5)
+  })
+})
+
+describe('calculateClockOutTime', () => {
+  test('should return the hour someone should clock out based on an 8 hour day', () => {
+    expect(calculateClockOutTime('9', '00').hours).toEqual(5)
   })
 })
