befc1b0c67e3e951a58bc2596c428eb80e7e23a0
Ted Weiss <tedweiss@Teds-MacBook-Pro.local>
2019-04-28 23:47:32 -0400

    add calculateClockOutTime function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+export const calculateClockOutTime = (startHours, startMinutes) => {
+  let hours = calculateClockOutHours(startHours)
+  let time = { hours, minutes: startMinutes }
+  return time
+}
+
 export const calculateClockOutHours = startHours => {
   let hours = parseInt(startHours) + 8 - 12
   return hours
