a5adb49e6ccf24fd7ee5de58b58775c760fce62e
Ted Weiss <tedweiss@Teds-MacBook-Pro.local>
2019-04-30 23:05:49 -0400

    determineAmPm test should return 'am' when a number before '12' is given

--- __tests__/src/utils.test.js
+++ __tests__/src/utils.test.js
@@ -1,4 +1,4 @@
-import { calculateClockOutHours, calculateClockOutTime } from '../../src/utils'
+import { calculateClockOutHours, calculateClockOutTime, determineAmPm } from '../../src/utils'
 
 describe('calculateClockOutHours', () => {
   test('should return the hour someone should clock out based on an 8 hour day', () => {
@@ -12,5 +12,11 @@ describe('calculateClockOutTime', () => {
   })
   test('should return the minute someone should clock out based on an 8 hour day', () => {
     expect(calculateClockOutTime('9', '30').minutes).toEqual(30)
+  })
+})
+
+describe('determineAmPm', () => {
+  test("should return 'am' when a number before '12' is given", () => {
+    expect(determineAmPm(7)).toEqual('am')
   })
 })
