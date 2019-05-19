bef9bff0e941dc265c1d3c9452d50e174f2ab820
Ted Weiss <tedweiss@Teds-MacBook-Pro.local>
2019-05-19 00:10:41 -0400

    add Minutes for Lunch input

--- src/components/TimePicker.js
+++ src/components/TimePicker.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import { DatePickerIOS, View } from 'react-native'
+import { DatePickerIOS, TextInput, View} from 'react-native'
 
 import DisplayClockOutTime from './DisplayClockOutTime'
-import { calculateClockOutTime } from '../utils'
 import ClockOutButton from './ClockOutButton'
+import { calculateClockOutTime } from '../utils'
 
 const TimePicker = () => {
   const [date, setDate] = useState(new Date())
@@ -11,11 +11,12 @@ const TimePicker = () => {
   const [minutes, setMinutes] = useState()
   const [amPm, setAmPm] = useState()
   const [displayTime, setDisplayTime] = useState(false)
+  const [lunchTime, setLunchTime] = useState(0)
   let dateHours = date.getHours()
   let dateMinutes = date.getMinutes()
 
   const updateTime = () => {
-    let clockOutTime = calculateClockOutTime(dateHours, dateMinutes)
+    let clockOutTime = calculateClockOutTime(dateHours, dateMinutes,lunchTime)
     const { amPm, hours, minutes } = clockOutTime
     let displayMinutes = minutes < 10 ? '0' + minutes : minutes
     setHours(hours.toString())
@@ -26,9 +27,35 @@ const TimePicker = () => {
   const handleDateChange = () => {
     displayTime ? setDisplayTime(false) : ''
   }
+  const handleChangeText = minutes => {
+    setLunchTime(minutes)
+  }
   return (
     <View style={{ width: 300 }}>
       <DatePickerIOS date={date} onDateChange={setDate} onChange={handleDateChange} mode={'time'} />
+      <View
+        style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          marginTop: 25
+        }}>
+        <TextInput
+          placeholder={'Minutes for Lunch'}
+          keyboardType={'numeric'}
+          onChangeText={handleChangeText}
+          style={{
+            textAlign: 'center',
+            paddingTop: 15,
+            paddingBottom: 15,
+            marginBottom: 25,
+            width: 238,
+            fontSize: 20,
+            borderWidth: 1,
+            borderColor: 'powderblue'
+          }}
+        />
+      </View>
       <ClockOutButton updateTime={updateTime} />
       <DisplayClockOutTime displayTime={displayTime} hours={hours} minutes={minutes} amPm={amPm} />
     </View>
